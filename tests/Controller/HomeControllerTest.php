46ebe4cacbb197eed9b6b06042b3238a2a3f1b68
armando <abpprkonsalting@gmail.com>
2021-12-03 11:16:09 -0500

    Rename WebTestController

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -4,7 +4,7 @@ namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class WebTestControllerTest extends WebTestCase
+class HomeControllerTest extends WebTestCase
 {
     public function testHomeRedirectToWeb()
     {
