a30f4d48c1720a93e9bd5a1661e95577f071232c
armando <abpprkonsalting@gmail.com>
2021-12-06 14:57:02 -0500

    improve ApiKeyAthenticationTest

--- tests/Security/ApiKeyAuthenticatorTest.php
+++ tests/Security/ApiKeyAuthenticatorTest.php
@@ -11,6 +11,7 @@ use Symfony\Component\Security\Core\Authentication\Token\NullToken;
 use Firebase\JWT\JWT;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAuthenticationException;;
 
 use App\Security\ApiKeyAuthenticator;
 
@@ -41,6 +42,21 @@ class ApiKeyAuthenticatorTest extends KernelTestCase
         $this->assertCount(1,$badges);
         $badge = array_pop($badges);
         $this->assertEquals($email,$badge->getUserIdentifier());
+    }
+
+    public function testNoTokenAuthenticate() 
+    {
+        $this->initialize();
+        $email = 'email@email.com';
+        $payload = [
+            "user" => $email,
+            "expire"  => (new \DateTime())->modify("+5 minutes")->getTimestamp(),
+        ];
+        $jwt = JWT::encode($payload, $this->params->get('jwt_secret'), 'HS256');
+        $request = new Request();
+        //$request->headers = new HeaderBag(['X-AUTH-TOKEN' => $jwt]);
+        $this->expectException(CustomUserMessageAuthenticationException::class);
+        $passport = $this->authenticator->authenticate($request);
     }
 
     public function testOnAuthenticationSuccess()
